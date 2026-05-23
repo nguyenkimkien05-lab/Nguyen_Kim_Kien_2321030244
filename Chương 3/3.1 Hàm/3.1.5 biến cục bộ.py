7e8c5fd59b79f6abc9b3704ede26601e8416688e
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 23:00:03 +0700

    Create 3.1.5 biến cục bộ.py

---
+++ Chương 3/3.1 Hàm/3.1.5 biến cục bộ.py
@@ -0,0 +1,10 @@
+# Bien cuc bo trong ham Python
+
+def show_message():
+    value = 20  # bien chi ton tai trong ham
+    print("Gia tri cua bien:", value)
+
+# Goi ham
+show_message()
+
+# Neu goi value o day se bi loi vi ngoai ham khong ton tai bien
