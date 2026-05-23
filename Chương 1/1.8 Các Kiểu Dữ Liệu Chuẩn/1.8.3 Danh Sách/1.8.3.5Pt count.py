089198c5b3d08c478780092ca118e618fd1ae6bb
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:30:04 +0700

    Create 1.8.3.5Pt count.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.5Pt count.py
@@ -0,0 +1,9 @@
+# Dem so lan xuat hien cua phan tu trong danh sach
+
+subjects = ["Math", "Python", "English", "Python"]
+
+# Dem so lan xuat hien cua Python
+print("Python xuat hien:", subjects.count("Python"))
+
+# Dem so lan xuat hien cua Math
+print("Math xuat hien:", subjects.count("Math"))
