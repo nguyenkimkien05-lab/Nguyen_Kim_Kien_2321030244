6436cf8cea9e6a98fba771deb483b96d388402c0
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:33:38 +0700

    Create 1.8.3.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.py
@@ -0,0 +1,12 @@
+# Xu ly chuoi trong Python
+
+text = "PythonCode1"
+
+# Hien thi ky tu thu 3
+print("Ky tu thu 3:", text[2])
+
+# Lay cac ky tu tu vi tri thu 4 den thu 7
+print("Doan ky tu tu 4 den 7:", text[3:7])
+
+# Lay chuoi tu ky tu thu 11 den cuoi
+print("Chuoi tu vi tri 11 tro di:", text[10:])
