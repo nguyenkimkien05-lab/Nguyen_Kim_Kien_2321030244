07807ec9bca2fad5738f9c14d06de2274ca454f7
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:59:34 +0700

    Create 3.1.4 Tham số mặc định.py

---
+++ Chương 3/3.1 Hàm/3.1.4 Tham số mặc định.py
@@ -0,0 +1,10 @@
+# Ham trong Python voi tham so mac dinh
+
+def add(a, b=10):
+    return a + b
+
+# Truyen du 2 tham so
+print(add(3, 7))
+
+# Chi truyen 1 tham so (b se lay gia tri mac dinh = 10)
+print(add(5))
