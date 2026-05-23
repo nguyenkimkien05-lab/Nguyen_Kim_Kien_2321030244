2edc15ae78e6edd2f3024b015f009d627117ca5a
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:35:30 +0700

    Create 1.8.4.1 Bài tập.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.4 Bộ (Tuple)/1.8.4.1 Bài tập.py
@@ -0,0 +1,10 @@
+# Nhap ma tran dang tuple trong Python
+
+# Nhap du lieu tu ban phim
+table_data = eval(input("Nhap ma tran tuple: "))
+
+# Hien thi tung dong cua ma tran
+print(list(table_data[0]))
+print(list(table_data[1]))
+print(list(table_data[2]))
+print(list(table_data[3]))
