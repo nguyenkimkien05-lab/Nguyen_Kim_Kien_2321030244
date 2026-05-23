17602323a36c18df959853b77076b46495c4f36f
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:22:49 +0700

    Update 1.8.3.13.py

--- Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.13.py
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.13.py
@@ -1,1 +1,13 @@
+# Sap xep danh sach trong Python
 
+numbers = [8, 5, 2, 9]
+
+# Sap xep tang dan
+numbers.sort()
+
+print("Danh sach tang dan:", numbers)
+
+# Sap xep giam dan
+numbers.sort(reverse=True)
+
+print("Danh sach giam dan:", numbers)
