73a0257dbbd013fd455156786627bafc632a0b82
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:24:12 +0700

    Update 1.8.3.1len.py

--- Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.1len.py
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.1len.py
@@ -1,1 +1,8 @@
+# Dem so phan tu trong danh sach
 
+data_1 = [10, 20, "Python"]
+data_2 = ["HTML", "CSS", "JavaScript", "SQL"]
+
+# Hien thi do dai cua tung danh sach
+print("So luong phan tu cua data_1:", len(data_1))
+print("So luong phan tu cua data_2:", len(data_2))
