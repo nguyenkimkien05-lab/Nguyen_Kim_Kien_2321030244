56f6870e5caf5f3c95cb6cdddfd9636076054536
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:07:02 +0700

    Add files via upload

--- Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.2 Kiểu Chuỗi/1.8.2.1.py
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.2 Kiểu Chuỗi/1.8.2.1.py
@@ -1,1 +1,21 @@
+# Lam viec voi chuoi trong Python
 
+message = "Python Programming"
+
+# Lay ky tu dau tien
+print(message[0])
+
+# Hien thi toan bo chuoi
+print(message[:])
+
+# Cat chuoi tu vi tri thu 2 den het
+print(message[1:])
+
+# Lay 6 ky tu dau
+print(message[:6])
+
+# Lay cac ky tu tu vi tri 0 den 5
+print(message[0:5])
+
+# Lay mot doan chuoi bat ky
+print(message[3:10])
