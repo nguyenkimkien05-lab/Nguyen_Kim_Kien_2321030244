895b09cdde76486025f1404a52acd5132e7a5cfe
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:05:02 +0700

    Add files via upload

--- Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.1 Kiểu Số/1.8.1.py
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.1 Kiểu Số/1.8.1.py
@@ -1,1 +1,29 @@
+# Minh hoa mot so ham trong thu vien math
 
+import math
+
+# Hien thi phan nguyen cua mot gia tri thuc
+value_1 = 15.68
+integer_part = math.trunc(value_1)
+
+print("Phan nguyen cua so:", integer_part)
+
+# Tim gia tri tuyet doi
+value_2 = -25
+absolute_number = math.fabs(value_2)
+
+print("Gia tri tuyet doi:", absolute_number)
+
+# Tinh can bac hai cua mot so
+value_3 = 81
+root_value = math.sqrt(value_3)
+
+print("Can bac hai cua so:", root_value)
+
+# Tim UCLN cua hai so nguyen
+first_num = 42
+second_num = 56
+
+greatest_common_divisor = math.gcd(first_num, second_num)
+
+print("Uoc chung lon nhat:", greatest_common_divisor)
