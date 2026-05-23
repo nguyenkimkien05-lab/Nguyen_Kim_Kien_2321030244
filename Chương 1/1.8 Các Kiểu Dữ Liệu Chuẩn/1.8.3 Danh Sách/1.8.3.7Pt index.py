d5011d56190a3028d3a96079b4e1aef34d214853
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:32:20 +0700

    Create 1.8.3.7Pt index.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.7Pt index.py
@@ -0,0 +1,8 @@
+# Tim vi tri cua phan tu trong danh sach
+
+subjects = ["Math", "Python", "Java", "SQL", "HTML"]
+
+# Tim chi muc cua phan tu "Java"
+position = subjects.index("Java")
+
+print("Vi tri cua Java trong list la:", position)
