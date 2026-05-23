cd9f6bc00ce3d4ed9305f7c6115071f44ceeff1d
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:47:28 +0700

    Create 2.1.3 lệnh if-elif-else vd1.py

---
+++ Chương 2/2.1 Lệnh if/2.1.3 lệnh if-elif-else vd1.py
@@ -0,0 +1,16 @@
+# Tra cuu ma dan toc trong Python
+
+code = int(input("Nhap ma dan toc (1-5): "))
+
+if code == 1:
+    print("Dan toc Kinh")
+elif code == 2:
+    print("Dan toc Tay")
+elif code == 3:
+    print("Dan toc Nung")
+elif code == 4:
+    print("Dan toc Thai")
+elif code == 5:
+    print("Dan toc Khmer")
+else:
+    print("Ma khong hop le")
