330f19faafcd44baedb318f81d10dff019e2a730
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 21:50:30 +0700

    Add files via upload

--- Chương 1/1.4 Cú Pháp Python/1.4.1.py
+++ Chương 1/1.4 Cú Pháp Python/1.4.1.py
@@ -1,1 +1,11 @@
+print("=== CHUONG TRINH PYTHON ===")
 
+gia_tri = 200
+
+print("Gia tri cua bien a:", gia_tri)
+
+loi_chao = "Xin chao cac ban sinh vien"
+khoa = "Khoa Cong nghe thong tin"
+
+print(loi_chao)
+print(khoa)
