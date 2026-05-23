e63e0c0da5403430820697e71c146e70a63e7616
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:28:24 +0700

    Update 1.8.3.3List.py

--- Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.3List.py
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.3List.py
@@ -1,1 +1,8 @@
+# Chuyen tuple thanh list trong Python
 
+languages_tuple = ("Python", "Java", "C#", "PHP")
+
+# Chuyen doi sang danh sach
+languages_list = list(languages_tuple)
+
+print(languages_list)
