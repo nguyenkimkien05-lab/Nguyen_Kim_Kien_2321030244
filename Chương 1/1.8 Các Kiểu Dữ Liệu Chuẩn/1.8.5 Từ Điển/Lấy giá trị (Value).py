b0d0026d4dcca56b2c7c6842fc634db959d9ecdc
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:38:47 +0700

    Create Lấy giá trị (Value).py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.5 Từ Điển/Lấy giá trị (Value).py
@@ -0,0 +1,10 @@
+# Truy cap gia tri trong dictionary Python
+
+person = {
+    "name": "Minh",
+    "member": 2024
+}
+
+# Lay gia tri theo key
+print(person["name"])     # Minh
+print(person["member"])   # 2024
