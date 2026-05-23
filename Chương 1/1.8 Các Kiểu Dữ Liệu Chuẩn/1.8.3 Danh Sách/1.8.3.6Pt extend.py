5bbeb6dc855bb04cd557e5c8176feffc603f414c
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:30:48 +0700

    Create 1.8.3.6Pt extend.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.6Pt extend.py
@@ -0,0 +1,9 @@
+# Noi hai danh sach trong Python
+
+frontend_languages = ["HTML", "CSS", "JavaScript"]
+backend_languages = ["Python", "PHP"]
+
+# Them tat ca phan tu cua list thu hai vao list thu nhat
+frontend_languages.extend(backend_languages)
+
+print("Danh sach sau khi ghep:", frontend_languages)
