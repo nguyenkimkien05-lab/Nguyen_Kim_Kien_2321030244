642130d0b6642b40c133881107052fda1883c88e
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:37:44 +0700

    Create Khởi tạo dld.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.5 Từ Điển/Khởi tạo dld.py
@@ -0,0 +1,14 @@
+# Tao va hien thi dictionary trong Python
+
+# Khoi tao dictionary co du lieu
+student_info = {
+    "name": "Kien",
+    "age": 20
+}
+
+print(student_info)
+
+# Tao dictionary rong
+new_dictionary = {}
+
+print(new_dictionary)
