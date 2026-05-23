5f0f073e235e240a467c5c5b7c6aadb494ad8088
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:46:35 +0700

    Create 2.1.2 Bài tập.py

---
+++ Chương 2/2.1 Lệnh if/2.1.2 Bài tập.py
@@ -0,0 +1,11 @@
+# So sanh hai so thuc trong Python
+
+a = float(input("Nhap so thu nhat: "))
+b = float(input("Nhap so thu hai: "))
+
+if a > b:
+    print("So lon hon la:", a)
+elif a < b:
+    print("So lon hon la:", b)
+else:
+    print("Hai so bang nhau")
