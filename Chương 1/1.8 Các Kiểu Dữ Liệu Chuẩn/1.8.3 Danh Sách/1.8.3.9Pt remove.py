a431e7df1ff627c520369971cd006798a7c6eb10
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:32:58 +0700

    Add Python script to remove an item from a list

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.9Pt remove.py
@@ -0,0 +1,8 @@
+# Xoa phan tu trong danh sach Python
+
+courses = ["HTML", "CSS", "JavaScript", "Python", "SQL"]
+
+# Xoa phan tu co gia tri "JavaScript"
+courses.remove("JavaScript")
+
+print(courses)
