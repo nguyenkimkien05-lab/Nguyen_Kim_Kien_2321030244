74fd4d1332f3283bec9838f227532884712034bd
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 23:05:59 +0700

    Create 3.3.4 pt readline.py

---
+++ Chương 3/3.2 Modul/3.3.4 pt readline.py
@@ -0,0 +1,13 @@
+# Doc file van ban trong Python
+
+file = open("toanco.txt", "r")
+
+# Doc dong 1
+line1 = file.readline()
+print("Dong 1:", line1)
+
+# Doc dong 2
+line2 = file.readline()
+print("Dong 2:", line2)
+
+file.close()
