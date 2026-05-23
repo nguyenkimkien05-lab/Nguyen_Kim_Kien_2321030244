c79fea90e39f6b9371977153f1b39d4bade3bfba
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:31:22 +0700

    Create 1.8.3.8Pt insert,pop.py

---
+++ Chương 1/1.8 Các Kiểu Dữ Liệu Chuẩn/1.8.3 Danh Sách/1.8.3.8Pt insert,pop.py
@@ -0,0 +1,13 @@
+# Chen va xoa phan tu trong danh sach
+
+tools = ["Photoshop", "Illustrator", "Canva"]
+
+# Chen phan tu moi vao vi tri thu 1
+tools.insert(1, "Figma")
+
+print("Danh sach sau khi chen:", tools)
+
+# Xoa phan tu cuoi cung
+tools.pop()
+
+print("Danh sach sau khi xoa:", tools)
