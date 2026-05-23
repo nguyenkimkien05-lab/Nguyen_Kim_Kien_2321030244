c31fa7953ddf358a5d4920d51ad7527162f4bf5e
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:00:05 +0700

    Add files via upload

--- Chương 1/1.7 Nhập Xuất/1.7.2.py
+++ Chương 1/1.7 Nhập Xuất/1.7.2.py
@@ -1,1 +1,13 @@
+# Nhap du lieu tu ban phim trong Python
 
+# Nhap mot so thuc
+so_thuc = float(input("Nhap so thuc: "))
+
+# Nhap mot so nguyen
+so_nguyen = int(input("Nhap so nguyen: "))
+
+# Tinh tong hai gia tri
+tong_hai_so = so_thuc + so_nguyen
+
+# Hien thi ket qua
+print("Ket qua sau khi cong =", tong_hai_so)
