ac71b1822749bc47d2c9fe27e2386114bb81b5cd
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:51:21 +0700

    Create Lệnh break.py

---
+++ Chương 2/2.2 Cấu trúc lặp While/Lệnh break.py
@@ -0,0 +1,12 @@
+# Vong lap while va lenh break trong Python
+
+count = 10
+
+while count > 0:
+    print("Gia tri hien tai:", count)
+
+    count -= 1  # giam gia tri
+
+    # dung vong lap khi gap dieu kien
+    if count == 5:
+        break
