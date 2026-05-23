13e23fe73e7b6af3eb63662f1bf54ba8d8605b8d
Kienzxje <nguyenkimkien05@gmail.com>
2026-05-23 22:45:48 +0700

    Create 2,1,2 Lệnh if-else vd1.py

---
+++ Chương 2/2.1 Lệnh if/2,1,2 Lệnh if-else vd1.py
@@ -0,0 +1,8 @@
+# Kiem tra so chan le trong Python
+
+number = int(input("Nhap vao mot so nguyen: "))
+
+if number % 2 == 0:
+    print(f"So {number} la so chan")
+else:
+    print(f"So {number} la so le")
